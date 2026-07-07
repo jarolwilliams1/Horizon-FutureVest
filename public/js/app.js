522463b3c674e7f1dbf80506acee12c787e0aac8
JarolWilliams <215430519+jarolwilliams1@users.noreply.github.com.>
2026-07-07 17:26:47 -0400

    dios cuida de mi

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,9 @@
-const agregarPais = document.getElementById("bt-agregar-pais")
+const NombrePais = document.getElementById("nombre-pais-input");
+const CodigoIsoPais = document.getElementById("ISO-pais-input");
+
+const datosPais = {
+  nombre: NombrePais.value,
+  codigoIso: CodigoIsoPais.value
+};
+
+module.exports = datosPais;
---
+++ services/AgregarPaisService.js
@@ -0,0 +1,5 @@
+const datosPais = require("../public/js/app.js");
+
+dato
+console.log(datosPais.Nombre);
+console.log(datosPais.CodigoIso);
